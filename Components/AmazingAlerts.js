948dd0487aa673e755ab77bdc91240cabee3da10
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-08-28 10:59:21 -0500

    Probando commits

--- Components/AmazingAlerts.js
+++ Components/AmazingAlerts.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator, Button } from 'react-native';
 import ModalBox from 'react-native-modalbox';
 import PropTypes from 'prop-types';
 import { Col, Row, Grid } from 'react-native-easy-grid';
@@ -8,13 +8,61 @@ import Styles from '../Styles/Styles';
 export default class AmazingAlerts extends Component {
   constructor(props) {
     super(props);
+    this.props = {
+      Tipo: 'aprobado',
+      Titulo: 'Probando Titulo',
+      Spinner: false,
+      Mostrar: true,
+      BotonCancelado: false,
+      TextoBotonCancelado: '',
+      TextoBotonConfirmado: 'OK',
+      onBotonCancelado: this.onCancelar,
+      onBotonConfirmado: this.onConfirmado,
+    }
+  }
+
+  onCancelar = () => {
+
+  }
+
+
+  onConfirmado = () => {
+
   }
 
   render() {
     return (
-      <Modal style={Styles.Modal} position='center' ref='Modal' isDisabled={false} backdropPressToClose={false} swipeToClose={false}>
+      <Modal isOpen={this.props.Mostrar} style={Styles.Modal} position='center' ref='Modal' isDisabled={false} backdropPressToClose={false} swipeToClose={false}>
         <Grid style={Styles.Flex}>
-
+          <Row size={1} style={Styles.Centrado}>
+            <Text>{this.props.Titulo}</Text>
+          </Row>
+          <Row size={2}>
+            <Row>
+              <Text>{this.props.Titulo}</Text>
+            </Row>
+            <Row>
+              {this.props.Spinner ? (
+                <ActivityIndicator></ActivityIndicator>
+              ) : (
+                  null
+                )}
+            </Row>
+          </Row>
+          <Row size={1}>
+            <Col size={1} style={Styles.Centrado}>
+              <Button style={Styles.Flex}>
+                <Text>{this.props.TextoBotonConfirmado}</Text>
+              </Button>
+            </Col>
+            {this.props.BotonCancelado ? (
+              <Col size={1} style={Styles.Centrado}>
+                <Button style={Styles.Flex}>
+                  <Text>{this.props.TextoBotonCancelado}</Text>
+                </Button>
+              </Col>
+            ) : null}
+          </Row>
         </Grid>
       </Modal>
     );
