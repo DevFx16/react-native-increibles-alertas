cf5847d647c41ee1bdbfc03353919370ae2e5a43
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-08-28 21:42:18 -0500

    Mejorando Modals

--- Components/AmazingAlerts.js
+++ Components/AmazingAlerts.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text, ActivityIndicator, Button } from 'react-native';
-import ModalBox from 'react-native-modalbox';
+import Modal from 'react-native-modalbox';
 import PropTypes from 'prop-types';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import Styles from '../Styles/Styles';
 
 export default class AmazingAlerts extends Component {
-  constructor(props) {
-    super(props);
-    this.props = {
+  constructor(state) {
+    super(state);
+    this.state = {
       Tipo: 'aprobado',
       Titulo: 'Probando Titulo',
-      Spinner: false,
+      Mensaje: 'Probando Mensaje Version Mejorada',
+      Spinner: true,
       Mostrar: true,
-      BotonCancelado: false,
-      TextoBotonCancelado: '',
+      BotonCancelado: true,
+      TextoBotonCancelado: 'Cancelar',
       TextoBotonConfirmado: 'OK',
       onBotonCancelado: this.onCancelar,
       onBotonConfirmado: this.onConfirmado,
-    }
+    };
   }
 
   onCancelar = () => {
@@ -32,34 +33,28 @@ export default class AmazingAlerts extends Component {
 
   render() {
     return (
-      <Modal isOpen={this.props.Mostrar} style={Styles.Modal} position='center' ref='Modal' isDisabled={false} backdropPressToClose={false} swipeToClose={false}>
+      <Modal isOpen={this.state.Mostrar} style={Styles.Modal} position='center' ref='Modal' isDisabled={false} backdropPressToClose={false} swipeToClose={false}>
         <Grid style={Styles.Flex}>
           <Row size={1} style={Styles.Centrado}>
-            <Text>{this.props.Titulo}</Text>
+            <Text style={Styles.TextoHeader}>{this.state.Titulo}</Text>
           </Row>
-          <Row size={2}>
-            <Row>
-              <Text>{this.props.Titulo}</Text>
+          <Col size={2} style={Styles.Centrado}>
+            {this.state.Spinner ? (
+              <Row style={Styles.Centrado}>
+                <ActivityIndicator size='large' color='violet' />
+              </Row>
+            ) : (null)}
+            <Row style={Styles.Centrado}>
+              <Text style={Styles.TextoMensaje}>{this.state.Mensaje}</Text>
             </Row>
-            <Row>
-              {this.props.Spinner ? (
-                <ActivityIndicator></ActivityIndicator>
-              ) : (
-                  null
-                )}
-            </Row>
-          </Row>
-          <Row size={1}>
-            <Col size={1} style={Styles.Centrado}>
-              <Button style={Styles.Flex}>
-                <Text>{this.props.TextoBotonConfirmado}</Text>
-              </Button>
+          </Col>
+          <Row size={0.5}>
+            <Col size={1}>
+              <Button style={[Styles.Boton, Styles.Flex]} title={this.state.TextoBotonConfirmado} color='blue'/>
             </Col>
-            {this.props.BotonCancelado ? (
-              <Col size={1} style={Styles.Centrado}>
-                <Button style={Styles.Flex}>
-                  <Text>{this.props.TextoBotonCancelado}</Text>
-                </Button>
+            {this.state.BotonCancelado ? (
+              <Col size={1}>
+                <Button style={[Styles.Boton, Styles.Flex]} title={this.state.TextoBotonCancelado} color='red'/>
               </Col>
             ) : null}
           </Row>
@@ -69,7 +64,7 @@ export default class AmazingAlerts extends Component {
   }
 }
 
-AmazingAlerts.propTypes = {
+/*AmazingAlerts.propTypes = {
   Tipo: PropTypes.string.isRequired,
   Titulo: PropTypes.string.isRequired,
   Spinner: PropTypes.bool.isRequired,
@@ -79,4 +74,4 @@ AmazingAlerts.propTypes = {
   TextoBotonConfirmado: PropTypes.string.isRequired,
   onBotonCancelado: PropTypes.func.isRequired,
   onBotonConfirmado: PropTypes.func.isRequired,
-}
+  } */
--- Styles/Styles.js
+++ Styles/Styles.js
@@ -2,8 +2,9 @@ import { StyleSheet, Dimensions } from 'react-native';
 
 export default Styles = StyleSheet.create({
     Modal: {
-        width: Dimensions.get('window').width - 60,
-        height: 160
+        width: Dimensions.get('window').width - 200,
+        height: 230,
+        borderRadius: 20,
     },
     Flex: {
         flex: 1
@@ -11,5 +12,17 @@ export default Styles = StyleSheet.create({
     Centrado: {
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    Boton: {
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        height: 150
+    },
+    TextoHeader: {
+        fontWeight: 'bold',
+        fontSize: 24
+    },
+    TextoMensaje: {
+        fontSize: 20
     }
 });
